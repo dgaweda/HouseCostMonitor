2d25621778d8aea2281a55479f588111a7eb0a6b
dgaweda <dgaweda1997@gmail.com>
2024-11-09 15:21:35 +0100

    Add Middlewar that will log request if request took longer than 4 sec

--- HouseCostMonitor.API/Middlewares/TimeLoggingMiddleware.cs
+++ HouseCostMonitor.API/Middlewares/TimeLoggingMiddleware.cs
@@ -1,14 +1,19 @@
 namespace HouseCostMonitor.API.Middlewares;
 
+using System.Diagnostics;
+
 public class TimeLoggingMiddleware(ILogger<TimeLoggingMiddleware> logger) : IMiddleware
 {
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
+        var timestamp = Stopwatch.GetTimestamp();
         await next.Invoke(context);
-    }
-
-    private void LogHttpRequestRunningLongerThan4Secs(HttpContext context)
-    {
-        logger.LogWarning($"{context.Request}");
+        var elapsedTime = Stopwatch.GetElapsedTime(timestamp);
+        
+        if(elapsedTime.Seconds >= 4)
+            logger.LogInformation("Request [HTTP {Verb}] at [{Path}] took {Time} ms", 
+                context.Request.Method, 
+                context.Request.Path, 
+                elapsedTime.Milliseconds);
     }
 }
